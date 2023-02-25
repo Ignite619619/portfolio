9026710a53152de925d09a5e0458d3ee65b55d47
Ignite619619 <rvpmails@gmail.com>
2023-02-25 06:25:30 +0530

    first commit

---
+++ src/component/Works.jsx
@@ -0,0 +1,171 @@
+import React from "react";
+import code from "../assets/code2.png";
+
+const Works = () => {
+  return (
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8 w-full flex justify-center items-center flex-col">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500">
+            Work
+          </p>
+          <p className="py-6 text-2xl">Check out some of my most recent work</p>
+        </div>
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${code})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
+              <span className=" text-lg font-bold text-white tracking-wider">
+                Keeper Application
+              </span>
+              <p className="text-center">
+                A Note-Keeper web application built with React
+              </p>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/Ignite619619/KeeperApp"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
+              <span className=" text-lg font-bold text-white tracking-wider">
+                Expense Tracker Application
+              </span>
+              <p className="text-center">
+                An expense tracker web application built with React
+              </p>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/Ignite619619/expense-tracker"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
+              <span className=" text-lg font-bold text-white tracking-wider">
+                Newsletter Signup Application
+              </span>
+              <p className="text-center">
+                A Newsletter-Signup web application built with React and Express
+              </p>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/Ignite619619/newsletter-signup"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
+              <span className=" text-lg font-bold text-white tracking-wider">
+                TinDog Application
+              </span>
+              <p className="text-center">A Tinder for Dog web application</p>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/Ignite619619/TinDog"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
+              <span className=" text-lg font-bold text-white tracking-wider">
+                Calculator Application
+              </span>
+              <p className="text-center">
+                A Calculator web application built with React
+              </p>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/Ignite619619/calculator"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
+              <span className=" text-lg font-bold text-white tracking-wider">
+                Drum Kit Application
+              </span>
+              <p className="text-center">
+                A Virtual Drum Kit web application built with JavaScript, HTML
+                and CSS
+              </p>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/Ignite619619/DrumChallenge"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Works;
